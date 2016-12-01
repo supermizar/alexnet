b25c1c95cd7d5d5e367e6e356a483b6880d28500
10171663 <qiu.ruitao@zte.com.cn>
2016-12-01 23:04:45 +0800

    feature: consider bias for full connect layer

--- python/fc_layer.py
+++ python/fc_layer.py
@@ -4,6 +4,7 @@ import numpy as np
 class FcLayer(object):
     def __init__(self, network, node_count, activator):
         self.output_array = np.zeros([node_count])
+        self.bias_array = np.zeros([node_count])
         self.input_shape = network.layers[-1].get_output().shape
         self.input_1dim = reduce(lambda ret, dim: ret * dim, self.input_shape, 1)
         self.trans_matrix = np.zeros([node_count, self.input_1dim])
@@ -15,7 +16,7 @@ class FcLayer(object):
 
     def forward(self, input_array):
         self.input_array = input_array.reshape(self.input_1dim)
-        self.output_array = np.dot(self.trans_matrix, self.input_array)
+        self.output_array = np.dot(self.trans_matrix, self.input_array) + self.bias_array
         self.output_array = np.array([self.activator.forward(value) for value in self.output_array])
 
     def calc_output_layer_delta(self, label):
@@ -28,6 +29,7 @@ class FcLayer(object):
 
     def update_weight(self, rate):
         self.trans_matrix += rate * np.dot(self.delta_array.reshape([len(self.delta_array),1]),self.input_array.reshape([1, self.input_1dim]))
+        self.bias_array += rate * self.delta_array
 
     def get_transformed_delta(self):
         return np.dot(self.trans_matrix.transpose(), self.delta_array)
