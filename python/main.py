4909c8dd5bef285872e1ddf23a95c93690e38893
10171663 <qiu.ruitao@zte.com.cn>
2016-12-05 22:43:59 +0800

    feature: add max pooling layer and test it with main

--- python/main.py
+++ python/main.py
@@ -3,13 +3,16 @@ from fc_layer import FcLayer
 from conv_layer import ConvLayer
 from conv_layer_hidden import ConvLayerHidden
 from activator import ReluActivator as Relu
+from max_pooling_layer import MaxPoolingLayer
 import numpy as np
 
 if __name__ == '__main__':
     fake_input = np.random.uniform(16,32,[3,50,50])
     net = Network()
     ConvLayer(net, 50, 50, 3, 11, 11, 48, 2, 4, Relu(), 0.05)
+    MaxPoolingLayer(net, 3, 3, 2)
     ConvLayerHidden(net, 3, 3, 24, 2, 3, Relu(), 0.05)
+    MaxPoolingLayer(net, 2, 2, 1)
     FcLayer(net, 5, Relu())
     FcLayer(net, 10, Relu())
     net.train_one_sample(np.ones([10]), fake_input, 0.1)
--- python/max_pooling_layer.py
+++ python/max_pooling_layer.py
@@ -1,41 +1,59 @@
 import numpy as np
+from utils import get_patch
+from utils import get_max_index
+
+
 class MaxPoolingLayer(object):
-    def __init__(self, input_width, input_height,
-                 channel_number, filter_width,
+    def __init__(self, network,filter_width,
                  filter_height, stride):
-        self.input_width = input_width
-        self.input_height = input_height
-        self.channel_number = channel_number
+        upstream_layer = network.layers[-1]
+        self.input_width = upstream_layer.output_width
+        self.input_height = upstream_layer.output_height
+        self.channel_number = upstream_layer.filter_number
         self.filter_width = filter_width
         self.filter_height = filter_height
         self.stride = stride
-        self.output_width = (input_width -
-            filter_width) / self.stride + 1
-        self.output_height = (input_height -
-            filter_height) / self.stride + 1
+        self.output_width = (self.input_width -
+                             filter_width) / self.stride + 1
+        self.output_height = (self.input_height -
+                              filter_height) / self.stride + 1
         self.output_array = np.zeros((self.channel_number,
-            self.output_height, self.output_width))
+                                      self.output_height, self.output_width))
+        network.append_layer(self)
+
     def forward(self, input_array):
+        self.input_array = input_array
         for d in range(self.channel_number):
             for i in range(self.output_height):
                 for j in range(self.output_width):
-                    self.output_array[d,i,j] = (
+                    self.output_array[d, i, j] = (
                         get_patch(input_array[d], i, j,
-                            self.filter_width,
-                            self.filter_height,
-                            self.stride).max())
-    def backward(self, input_array, sensitivity_array):
-        self.delta_array = np.zeros(input_array.shape)
+                                  self.filter_width,
+                                  self.filter_height,
+                                  self.stride).max())
+
+    def calc_layer_delta(self, downstream_layer):
+        self.delta_array = np.zeros(self.input_array.shape)
+        sensitivity_array = downstream_layer.get_transformed_delta().reshape(self.output_array.shape)
         for d in range(self.channel_number):
             for i in range(self.output_height):
                 for j in range(self.output_width):
                     patch_array = get_patch(
-                        input_array[d], i, j,
+                        self.input_array[d], i, j,
                         self.filter_width,
                         self.filter_height,
                         self.stride)
                     k, l = get_max_index(patch_array)
                     self.delta_array[d,
-                        i * self.stride + k,
-                        j * self.stride + l] = \
-                        sensitivity_array[d,i,j]
+                                     i * self.stride + k,
+                                     j * self.stride + l] = \
+                        sensitivity_array[d, i, j]
+
+    def get_output(self):
+        return self.output_array
+
+    def get_transformed_delta(self):
+        return self.delta_array
+
+    def update_weight(self, rate):
+        pass
